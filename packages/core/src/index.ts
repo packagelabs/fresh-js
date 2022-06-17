2e9fb10ce6fe5c3d36245d9836ac64a11549cc0f
Peter Siemens <peterjsiemens@gmail.com>
2022-06-17 11:35:37 +0100

    Initial version of @fresh-js/core

---
+++ packages/core/src/index.ts
@@ -0,0 +1,39 @@
+import * as fcl from '@onflow/fcl';
+import { Signer } from '@fresh-js/crypto';
+
+export interface TransactionOptions {
+  payer: Authorizer;
+  proposer: Authorizer;
+  authorizers: [Authorizer];
+}
+
+const toHex = (buffer: Buffer) => buffer.toString('hex');
+const fromHex = (hex: string) => Buffer.from(hex, 'hex');
+
+export class Authorizer {
+  address: string;
+  keyIndex: number;
+  signer: Signer;
+
+  constructor({ address, keyIndex, signer }: { address: string; keyIndex: number; signer: Signer }) {
+    this.address = address;
+    this.keyIndex = keyIndex;
+    this.signer = signer;
+  }
+
+  toFCLAuthorizationFunction() {
+    return async (account = {}) => {
+      return {
+        ...account,
+        tempId: 'SIGNER',
+        addr: fcl.sansPrefix(this.address),
+        keyId: this.keyIndex,
+        signingFunction: (data: { message: string }) => ({
+          addr: fcl.withPrefix(this.address),
+          keyId: this.keyIndex,
+          signature: toHex(this.signer.sign(fromHex(data.message))),
+        }),
+      };
+    };
+  }
+}
