39d8d1092410ef13d4f0790fe54860dc4e4bda26
Peter Siemens <peterjsiemens@gmail.com>
2022-06-20 16:41:26 -0700

    Update index.test.ts

--- packages/nft/src/index.test.ts
+++ packages/nft/src/index.test.ts
@@ -1,19 +1,44 @@
-import * as fcl from '@onflow/fcl';
 import { HashAlgorithm, InMemoryECPrivateKey, InMemoryECSigner, SignatureAlgorithm } from '@fresh-js/crypto';
-import { OnChainMintRevealProject, Field, fieldTypes } from './index';
+import { NetworkConfig, OnChainMintRevealProject, Field, fieldTypes } from './index';
 import { Authorizer } from '@fresh-js/core';
 
 describe('OnChainMintRevealProject', () => {
-  fcl.config().put('accessNode.api', 'http://localhost:8888');
+  // Emulator configuration
+  const networkConfig = new NetworkConfig({
+    host: 'http://localhost:8888',
+    contracts: {
+      FungibleToken: '0xee82856bf20e2aa6',
+      NonFungibleToken: '0xf8d6e0586b0a20c7',
+      MetadataViews: '0xf8d6e0586b0a20c7',
+    },
+  });
 
   const project = new OnChainMintRevealProject({
     contractName: 'Foo',
-    contractAddress: '0xf8d6e0586b0a20c7',
+    contractAddress: '0x01cf0e2f2f715450',
     fields: [new Field('foo', fieldTypes.String), new Field('bar', fieldTypes.Int)],
+    networkConfig,
   });
+
+  const PRIVATE_KEY_HEX = '4d9287571c8bff7482ffc27ef68d5b4990f9bd009a1e9fa812aae08ba167d57f';
+
+  const privateKey = InMemoryECPrivateKey.fromHex(PRIVATE_KEY_HEX, SignatureAlgorithm.ECDSA_P256);
+  const signer = new InMemoryECSigner(privateKey, HashAlgorithm.SHA3_256);
+
+  const authorizer = new Authorizer({ address: '0xf8d6e0586b0a20c7', keyIndex: 0, signer });
+
+  project.setDefaultAuthorizer(authorizer);
 
   it('should generate a contract', async () => {
     console.log(await project.getContract());
+  });
+
+  it('should deploy a contract', async () => {
+    const publicKey = privateKey.getPublicKey();
+
+    const contractAddress = await project.deployContract(publicKey, HashAlgorithm.SHA3_256, 'foo', {});
+
+    console.log(contractAddress);
   });
 
   it('should mint and reveal NFTs', async () => {
@@ -34,18 +59,13 @@ describe('OnChainMintRevealProject', () => {
       },
     ];
 
-    const PRIVATE_KEY_HEX = '4d9287571c8bff7482ffc27ef68d5b4990f9bd009a1e9fa812aae08ba167d57f';
-
-    const privateKey = InMemoryECPrivateKey.fromHex(PRIVATE_KEY_HEX, SignatureAlgorithm.ECDSA_P256);
-    const signer = new InMemoryECSigner(privateKey, HashAlgorithm.SHA3_256);
-
-    const authorizer = new Authorizer({ address: '0xf8d6e0586b0a20c7', keyIndex: 0, signer });
-
     const mintedNFTs = await project.mintNFTs(metadata, {
       payer: authorizer,
       proposer: authorizer,
       authorizers: [authorizer],
     });
+
+    console.log(mintedNFTs);
 
     const revealedNFTs = await project.revealNFTs(mintedNFTs, {
       payer: authorizer,
