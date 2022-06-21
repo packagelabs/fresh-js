50fb7fdfd1897dc1be7fe511f30350a9db9a3dfa
Peter Siemens <peterjsiemens@gmail.com>
2022-06-20 17:17:05 -0700

    Add NetworkConfig to @fresh-js/core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -8,12 +8,6 @@ export type Event = {
   data: { [key: string]: string };
 };
 
-export interface TransactionOptions {
-  payer: Authorizer;
-  proposer: Authorizer;
-  authorizers: [Authorizer];
-}
-
 const toHex = (buffer: Buffer) => buffer.toString('hex');
 const fromHex = (hex: string) => Buffer.from(hex, 'hex');
 
@@ -44,3 +38,31 @@ export class Authorizer {
     };
   }
 }
+
+export class NetworkConfig {
+  host: string;
+  contracts: { [key: string]: string };
+
+  constructor({ host, contracts }: { host: string; contracts: { [key: string]: string } }) {
+    this.host = host;
+    this.contracts = contracts;
+  }
+}
+
+export const TestnetConfig = new NetworkConfig({
+  host: 'https://rest-testnet.onflow.org',
+  contracts: {
+    FungibleToken: '0x9a0766d93b6608b7',
+    NonFungibleToken: '0x631e88ae7f1d7c20',
+    MetadataViews: '0x631e88ae7f1d7c20',
+  },
+});
+
+export const MainnetConfig = new NetworkConfig({
+  host: 'https://rest-mainnet.onflow.org',
+  contracts: {
+    FungibleToken: '0xf233dcee88fe0abe',
+    NonFungibleToken: '0x1d7e57aa55817448',
+    MetadataViews: '0x1d7e57aa55817448',
+  },
+});
