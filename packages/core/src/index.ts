90ee3648d8259d71fec827e1d5a7002af2f2abc4
Peter Siemens <peterjsiemens@gmail.com>
2022-06-21 12:31:14 -0700

    @fresh-js/core - v0.0.4

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,34 @@ import * as fcl from '@onflow/fcl';
 
 import { Signer } from '@fresh-js/crypto';
 
+export class Config {
+  host: string;
+  contracts: { [key: string]: string };
+
+  constructor({ host, contracts }: { host: string; contracts: { [key: string]: string } }) {
+    this.host = host;
+    this.contracts = contracts;
+  }
+}
+
+export const TestnetConfig = new Config({
+  host: 'https://rest-testnet.onflow.org',
+  contracts: {
+    FungibleToken: '0x9a0766d93b6608b7',
+    NonFungibleToken: '0x631e88ae7f1d7c20',
+    MetadataViews: '0x631e88ae7f1d7c20',
+  },
+});
+
+export const MainnetConfig = new Config({
+  host: 'https://rest-mainnet.onflow.org',
+  contracts: {
+    FungibleToken: '0xf233dcee88fe0abe',
+    NonFungibleToken: '0x1d7e57aa55817448',
+    MetadataViews: '0x1d7e57aa55817448',
+  },
+});
+
 export type Event = {
   type: string;
   data: { [key: string]: string };
@@ -38,31 +66,3 @@ export class Authorizer {
     };
   }
 }
-
-export class NetworkConfig {
-  host: string;
-  contracts: { [key: string]: string };
-
-  constructor({ host, contracts }: { host: string; contracts: { [key: string]: string } }) {
-    this.host = host;
-    this.contracts = contracts;
-  }
-}
-
-export const TestnetConfig = new NetworkConfig({
-  host: 'https://rest-testnet.onflow.org',
-  contracts: {
-    FungibleToken: '0x9a0766d93b6608b7',
-    NonFungibleToken: '0x631e88ae7f1d7c20',
-    MetadataViews: '0x631e88ae7f1d7c20',
-  },
-});
-
-export const MainnetConfig = new NetworkConfig({
-  host: 'https://rest-mainnet.onflow.org',
-  contracts: {
-    FungibleToken: '0xf233dcee88fe0abe',
-    NonFungibleToken: '0x1d7e57aa55817448',
-    MetadataViews: '0x1d7e57aa55817448',
-  },
-});
