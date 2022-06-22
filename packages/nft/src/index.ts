9a6a063706e706680ea49f2c03127afcbebb04b3
Peter Siemens <peterjsiemens@gmail.com>
2022-06-21 22:59:01 -0700

    Export OnChainMinter from index

--- packages/nft/src/index.ts
+++ packages/nft/src/index.ts
@@ -1,7 +1,10 @@
-import OnChainBlindMinter from './minters/OnChainBlindMinter';
-export { OnChainBlindMinter };
-
 import Project from './Project';
+
 export { Project };
+
+import OnChainMinter from './minters/OnChainMinter';
+import OnChainBlindMinter from './minters/OnChainBlindMinter';
+
+export { OnChainMinter, OnChainBlindMinter };
 
 export * as schema from './schema';
