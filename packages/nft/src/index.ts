0f1e5715bf9dab337afb97a4d013126c6c511026
Peter Siemens <peterjsiemens@gmail.com>
2022-07-20 16:50:41 -0700

    Export Cadence generators

--- packages/nft/src/index.ts
+++ packages/nft/src/index.ts
@@ -3,8 +3,17 @@ import OnChainBlindCollection from './collections/OnChainBlindCollection';
 
 export { OnChainCollection, OnChainBlindCollection };
 
+import OnChainGenerator from './generators/OnChainGenerator';
+import OnChainBlindGenerator from './generators/OnChainBlindGenerator';
+
+export { OnChainGenerator, OnChainBlindGenerator };
+
 import ClaimSale from './sales/ClaimSale';
 
 export { ClaimSale };
+
+import ClaimSaleGenerator from './generators/ClaimSaleGenerator';
+
+export { ClaimSaleGenerator };
 
 export * as metadata from './metadata';
